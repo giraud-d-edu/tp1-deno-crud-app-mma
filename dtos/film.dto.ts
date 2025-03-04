991f5c9892c4fc159d4fd36ada761f929996046b
Mattis <mattisfibi46@gmail.com>
2025-03-04 08:32:28 +0100

    Feat: dto actor

---
+++ dtos/film.dto.ts
@@ -0,0 +1,14 @@
+import { z } from "https://deno.land/x/zod@v3.24.2/mod.ts";
+
+export const BookDto = z.object({
+    title: z.string().min(1, "Title is required"),
+    category: z.array(z.string()).nonempty("At least one category is required"),
+    actors: z.array(
+      z.object({
+        id: z.number().int().positive("ID must be a positive integer"),
+        role: z.string().min(1, "Role is required"),
+      })
+    ).nonempty("At least one actor is required"),
+  });
+
+export type BookDtoType = z.infer<typeof BookDto>;
--- dtos/note.dto.ts
+++ dtos/note.dto.ts
@@ -1,9 +1,9 @@
 import { z } from "https://deno.land/x/zod@v3.24.2/mod.ts";
 
 export const NoteDto = z.object({
-    id_film: z.number(),
-    user: z.string(),
-    note: z.number(),
+    id_film: z.number().int().positive("ID must be a positive integer"),
+    user: z.string().min(1, "User is required"),
+    note: z.number().int().min(0).max(5, "Note must be between 0 and 5"),
 })
 
 export type NoteDtoType = z.infer<typeof NoteDto>;
